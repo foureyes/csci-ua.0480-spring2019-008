8fbf17ac0b7b26ce975c8738da26878dd975d732
Joe Versoza <jversoza@cs.nyu.edu>
2019-05-06 19:38:26 +0000

    add auth

--- examples/auth/app.js
+++ examples/auth/app.js
@@ -24,54 +24,6 @@ app.use(function(req, res, next){
   next();
 });
 
-// register
-
-app.get('/register', (req, res) => {
-  res.render('register');
-});
-// async await - define async functions
-// within those functions... instead of using
-// regular promise syntax
-// await a promise... and consequently have an
-// awaited statement return the value that the
-// promise would have been resolved with
-app.post('/register', (req, res) => {
-  const {username, password} = req.body;
-  async function register(username, password) {
-    const found = await User.findOne({username: username});
-    if(found === null) {
-      // salt is generated for us 
-      // salt + password is hashed
-      const hash = await argon2.hash(password);
-
-      // salt is stored within the sash (so 
-      // don't have to save explicitly
-      const u = new User({
-        username: username, 
-        password: hash, 
-      });
-
-      const result = await u.save();
-      // you may want to login immediatly or add to 
-      // session
-      res.redirect('/');
-    } else {
-      res.send(found + ' using async and await');
-    }
-  }
-
-  register(username, password)
-});
-
-
-
-
-
-
-
-
-
-/*
 app.get('/', (req, res) => {
   res.render('index');
 });
@@ -82,25 +34,42 @@ app.get('/register', (req, res) => {
 
 app.post('/register', (req, res) => {
   const {username, password} = req.body;
+
+  // use async await to define asynchronous functions:
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
+ 
+  // within those functions... instead of using regular promise syntax
+  // an await expression can be used to capture the value of a resolved promise
+  // using simple variable assignment
   async function register(username, password) {
     try {
-      const user = await User.findOne({username: username})
-      if(!user) {
+      // TODO: this should be a case insenstivie search!
+      const found = await User.findOne({username: username});
+      if(found === null) {
+        // salt is generated for us 
+        // salt + password is hashed
         const hash = await argon2.hash(password);
-        const u = new User({username: username, password: hash});
-        const result = u.save();
+
+        // salt is stored within the sash (so 
+        // don't have to save explicitly
+        const u = new User({ username: username, password: hash});
+
+        const result = await u.save();
+
+        // you may want to login immediatly or add to session at this point
+        
         res.redirect('/');
       } else {
         console.log('duplicate username'); 
-        res.render('register');
-      
+        res.render('register', {error: true});
       }
-    } catch (err) {
+    } catch(err) {
        console.log(err); 
-       res.render('register');
+       res.render('register', {error: true});
     }
   }
-  register(username, password);
+
+  register(username, password)
 });
 
 app.get('/login', (req, res) => {
@@ -111,24 +80,23 @@ app.post('/login', (req, res) => {
   const {username, password} = req.body;
   async function login(username, password) {
     try {
-      // case insensitive
+      // TODO: this should be a case insenstivie search!
       const user = await User.findOne({username: username})
       const passwordMatch = await argon2.verify(user.password, password);
       if(passwordMatch) {
         req.session.username = user.username;
         res.redirect('/');
       } else {
-        console.log('not found', hash); 
-        res.render('login');
+        console.log('username or password did not match'); 
+        res.render('login', {error: true});
       }
     } catch (err) {
        console.log(err); 
-       res.render('login');
+       res.render('login', {error: true});
     }
   }
   login(username, password);
 });
-*/
 
 
 app.listen(3000);
